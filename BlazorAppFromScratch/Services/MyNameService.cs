40c46b0ca0f67d6e15293e36ffb865d7da6bb0f1
William Gecawich <bgecawich@smartmobilestrategies.com>
2019-12-17 19:52:00 -0500

    Intro Razor Components

---
+++ BlazorAppFromScratch/Services/MyNameService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorAppFromScratch.Services
+{
+    public class MyNameService : IMyNameService
+    {
+        public string GetMyName()
+        {
+            return "Bill's Counter Component";
+        }
+    }
+}
